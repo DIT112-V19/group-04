2020d80f7641f29f5ddfc92e2b95ddbfe518bca5
Konrad <gusottko@student.gu.se>
2019-04-03 15:20:11 +0200

    initial commit for basic  obstacle avoidance on the arudino

---
+++ arduino/obstacleAvoidance/constants.h
@@ -0,0 +1,18 @@
+// header file defining global constants
+
+#ifndef CONSTANTS_H
+
+#define CONSTANTS_H
+
+// include the connected pins
+#include "usedpins.h"
+
+// ultra sound settings
+#define US_MAX_DISTANCE	100
+
+// serial communication settings
+#define BAUD_RATE     9600
+
+#endif
+
+
---
+++ arduino/obstacleAvoidance/usedpins.h
@@ -0,0 +1,24 @@
+// header-file defining the aruduino pins used 
+// for the different sensors and actuators
+
+#ifndef USEDPINS_H
+#define USEDPINS_H
+
+// left motor pins
+#define BRUSHED_LEFT_FORWARD_PIN	8
+#define BRUSHED_LEFT_BACKWARD_PIN	10
+#define BRUSHED_LEFT_ENABLE_PIN		9
+
+// right motor pins
+#define BRUSHED_RIGHT_FORWARD_PIN	12
+#define BRUSHED_RIGHT_BACKWARD_PIN	13
+#define BRUSHED_RIGHT_ENABLE_PIN	11
+
+// ultra sound pins
+#define US_TRIGGER_PIN	5
+#define US_ECHO_PIN		4
+
+// bluetooth pins
+// TODO
+
+#endif
