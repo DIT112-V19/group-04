b46d5cbefbe72c54b72d3dfb5cc343d7b8a82ea0
bartekspitza <bartekspitza@hotmail.com>
2019-04-30 15:04:30 +0200

    fixes #7
    simple ios app built with swift that can talk to our server

---
+++ app/CarInterface/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CarInterface
+//
+//  Created by Maikzy on 2019-04-02.
+//  Copyright © 2019 TeamLate. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ViewController: UIViewController {
+
+    private final let SERVER_URL = "https://carpool.serveo.net/"
+    private final let MOVE_ENDPOINT = "move"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Tests the root endpoint of server to make sure we can access it
+        sendRequest(method: "GET", endPoint: "")
+    }
+
+    @IBAction func forward(_ sender: Any) {
+        sendRequest(method: "GET", endPoint: MOVE_ENDPOINT)
+    }
+    
+    private func sendRequest(method: String, endPoint: String) {
+        let url = URL(string: SERVER_URL + endPoint)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, res, er) in
+            if let data = data {
+                print(data.description)
+            }
+        }
+        task.resume()
+    }
+}
+
