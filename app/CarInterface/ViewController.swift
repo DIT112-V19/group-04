2ca780fd27eafa3a149105116d887e2c279f3378
ih8c01d <jeanpaull@live.it>
2019-05-05 21:12:55 +0200

    #22 enable zooming in for the map view. Add limits to zooming out. add ability to create an anotation according to the user tap. Add function to clear the view.

--- app/CarInterface/ViewController.swift
+++ app/CarInterface/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import MapKit
 import CoreLocation
 
+fileprivate let MERCATOR_OFFSET: Double = 268435456
+fileprivate let MERCATOR_RADIUS: Double = 85445659.44705395
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var rButton: UIButton!
@@ -21,14 +24,22 @@ class ViewController: UIViewController {
     let regionInMeters: Double = 1000
     var directionsArray: [MKDirections] = []
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         checkLocationServices()
+        mapView.delegate = self
+        
+        let singleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(longTap))
+        singleTapRecognizer.delegate = self
+        mapView.addGestureRecognizer(singleTapRecognizer)
         
         // Tests the root endpoint of server to make sure we can access it
         sendRequest(method: "GET", endPoint: "")
     }
 
+    
+    
     @IBAction func rButtonTapped(_ sender: Any) {
         initLindHolmenMap()
     }
@@ -47,6 +58,22 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    
+    @objc func longTap(sender: UIGestureRecognizer){
+        debugPrint("long tap")
+        let locationInView = sender.location(in: mapView)
+        let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+        addAnnotation(location: locationOnMap)
+       
+    }
+    
+    func addAnnotation(location: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Some Title"
+        annotation.subtitle = "Some Subtitle"
+        self.mapView.addAnnotation(annotation)
+    }
    
     
     func checkLocationServices() {
@@ -112,17 +139,24 @@ class ViewController: UIViewController {
     
     func initLindHolmenMap(){
         let lindHolmenLoc = CLLocation(latitude: 57.708604, longitude: 11.938750)
-        let regionRadius = CLLocationDistance(exactly: 1000)!
+        let regionRadius = CLLocationDistance(exactly: 1200)!
         let linHolmenRegion = MKCoordinateRegion(center:lindHolmenLoc.coordinate, latitudinalMeters: regionRadius
                 , longitudinalMeters: regionRadius)
         let edgeInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15);
         mapView.setVisibleMapRect(MKMapRectForCoordinateRegion(region: linHolmenRegion), edgePadding: edgeInsets, animated: true)
         
         mapView.isScrollEnabled = false
-        mapView.isZoomEnabled = false
+        mapView.isZoomEnabled = true
         mapView.isRotateEnabled = false
     }
     
+    
+    
+    
+    func clearView() {
+        let allAnnotations = self.mapView.annotations
+        self.mapView.removeAnnotations(allAnnotations)
+    }
     
     func MKMapRectForCoordinateRegion(region:MKCoordinateRegion) -> MKMapRect {
         let topLeft = CLLocationCoordinate2D(latitude: region.center.latitude + (region.span.latitudeDelta/2), longitude: region.center.longitude - (region.span.longitudeDelta/2))
@@ -170,7 +204,149 @@ extension MKCoordinateRegion{
 }
 
 extension MKMapView {
+   
+    
     func setVisibleRegion(mapRegion: MKCoordinateRegion, edgePadding insets: UIEdgeInsets, animated animate: Bool) {
         self.setVisibleMapRect(mapRegion.mapRect, edgePadding: insets , animated: animate)
+    }
+    
+    func getZoomLevel() -> Double {
+        
+        let reg = self.region
+        let span = reg.span
+        let centerCoordinate = reg.center
+        
+        // Get the left and right most lonitudes
+        let leftLongitude = centerCoordinate.longitude - (span.longitudeDelta / 2)
+        let rightLongitude = centerCoordinate.longitude + (span.longitudeDelta / 2)
+        let mapSizeInPixels = self.bounds.size
+        
+        // Get the left and right side of the screen in fully zoomed-in pixels
+        let leftPixel = self.longitudeToPixelSpaceX(longitude: leftLongitude)
+        let rightPixel = self.longitudeToPixelSpaceX(longitude: rightLongitude)
+        let pixelDelta = abs(rightPixel - leftPixel)
+        
+        let zoomScale = Double(mapSizeInPixels.width) / pixelDelta
+        let zoomExponent = log2(zoomScale)
+        let zoomLevel = zoomExponent + 20
+        
+        return zoomLevel
+    }
+    
+    func setCenter(coordinate: CLLocationCoordinate2D, zoomLevel: Int, animated: Bool) {
+        
+        let zoom = min(zoomLevel, 28)
+        
+        let span = self.coordinateSpan(centerCoordinate: coordinate, zoomLevel: zoom)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        self.setRegion(region, animated: true)
+    }
+    
+    // MARK: - Private func
+    
+    private func coordinateSpan(centerCoordinate: CLLocationCoordinate2D, zoomLevel: Int) -> MKCoordinateSpan {
+        
+        // Convert center coordiate to pixel space
+        let centerPixelX = self.longitudeToPixelSpaceX(longitude: centerCoordinate.longitude)
+        let centerPixelY = self.latitudeToPixelSpaceY(latitude: centerCoordinate.latitude)
+        
+        // Determine the scale value from the zoom level
+        let zoomExponent = 20 - zoomLevel
+        let zoomScale = NSDecimalNumber(decimal: pow(2, zoomExponent)).doubleValue
+        
+        // Scale the map’s size in pixel space
+        let mapSizeInPixels = self.bounds.size
+        let scaledMapWidth = Double(mapSizeInPixels.width) * zoomScale
+        let scaledMapHeight = Double(mapSizeInPixels.height) * zoomScale
+        
+        // Figure out the position of the top-left pixel
+        let topLeftPixelX = centerPixelX - (scaledMapWidth / 2)
+        let topLeftPixelY = centerPixelY - (scaledMapHeight / 2)
+        
+        // Find delta between left and right longitudes
+        let minLng: CLLocationDegrees = self.pixelSpaceXToLongitude(pixelX: topLeftPixelX)
+        let maxLng: CLLocationDegrees = self.pixelSpaceXToLongitude(pixelX: topLeftPixelX + scaledMapWidth)
+        let longitudeDelta: CLLocationDegrees = maxLng - minLng
+        
+        // Find delta between top and bottom latitudes
+        let minLat: CLLocationDegrees = self.pixelSpaceYToLatitude(pixelY: topLeftPixelY)
+        let maxLat: CLLocationDegrees = self.pixelSpaceYToLatitude(pixelY: topLeftPixelY + scaledMapHeight)
+        let latitudeDelta: CLLocationDegrees = -1 * (maxLat - minLat)
+        
+        return MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+    }
+    
+    private func longitudeToPixelSpaceX(longitude: Double) -> Double {
+        return round(MERCATOR_OFFSET + MERCATOR_RADIUS * longitude * .pi / 180.0)
+    }
+    
+    private func latitudeToPixelSpaceY(latitude: Double) -> Double {
+        if latitude == 90.0 {
+            return 0
+        } else if latitude == -90.0 {
+            return MERCATOR_OFFSET * 2
+        } else {
+            return round(MERCATOR_OFFSET - MERCATOR_RADIUS * Double(logf((1 + sinf(Float(latitude * .pi) / 180.0)) / (1 - sinf(Float(latitude * .pi) / 180.0))) / 2.0))
+        }
+    }
+    
+    private func pixelSpaceXToLongitude(pixelX: Double) -> Double {
+        return ((round(pixelX) - MERCATOR_OFFSET) / MERCATOR_RADIUS) * 180.0 / .pi
+    }
+    
+    
+    private func pixelSpaceYToLatitude(pixelY: Double) -> Double {
+        return (.pi / 2.0 - 2.0 * atan(exp((round(pixelY) - MERCATOR_OFFSET) / MERCATOR_RADIUS))) * 180.0 / .pi
+    }
+    
+    
+    
+}
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        print("Zoom: \(mapView.getZoomLevel())")
+        if mapView.getZoomLevel() < 13 {
+            mapView.setCenter(coordinate: mapView.centerCoordinate, zoomLevel: 14, animated: true)
+        }
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+            pinView!.pinTintColor = UIColor.black
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("tapped on pin ")
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let _ = view.annotation?.title! {
+                print("do something")
+            }
+        }
+    }
+    
+    
+}
+
+extension ViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return !(touch.view is MKPinAnnotationView)
     }
 }
