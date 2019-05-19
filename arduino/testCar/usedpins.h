9a68830e4a5368a9036201aff62591235b9fdcd7
Konrad <gusottko@student.gu.se>
2019-05-19 17:32:11 +0200

    use constants for used pins and global settings

---
+++ arduino/testCar/usedpins.h
@@ -0,0 +1,20 @@
+#ifndef USEDPINS_H
+#define USEDPINS_H
+
+// left motor pins
+const int BRUSHED_LEFT_FORWARD_PIN = 8;
+const int BRUSHED_LEFT_BACKWARD_PIN = 10;
+const int BRUSHED_LEFT_ENABLE_PIN = 9;
+
+// right motor pins
+const int BRUSHED_RIGHT_FORWARD_PIN = 12;
+const int BRUSHED_RIGHT_BACKWARD_PIN = 13;
+const int BRUSHED_RIGHT_ENABLE_PIN = 11;
+
+// ultra sound pins
+const int US_TRIGGER_PIN = 5;
+const int US_ECHO_PIN = 4;
+// bluetooth pins
+// TODO
+
+#endif
